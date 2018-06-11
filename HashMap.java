db607dc87a8b56d71cbdfc72162cae4b9642b73d
IrinaBabich <36416921+IrinaBabich@users.noreply.github.com>
2018-06-12 00:53:33 +0300

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -5,42 +5,54 @@ import java.util.ArrayList;
 public class HashMap implements Map {
     private static final int INITIAL_CAPACITY = 5;
     private int size;
-    private ArrayList<Entry>[] buckets = new ArrayList[INITIAL_CAPACITY];
+    private ArrayList<Entry>[] bucket;
+
+    public HashMap() {
+        this(INITIAL_CAPACITY);
+    }
+
+    public HashMap(int bucketCount) {
+        if (bucketCount <= 0) {
+            throw new IllegalArgumentException("Bucket size should be > 0 , bucket count =" + bucketCount);
+        }
+        this.bucket = new ArrayList[bucketCount];
+        for (int i = 0; i < bucketCount; i++) {
+            bucket[i] = new ArrayList<>();
+        }
+    }
+
 
     @Override
     // если у нас уже был ключ и значение, то когда помещаем новое значение,
     // происходит апдейт и метод нам возвращает новое значение
     public Object put(Object key, Object value) {
-        boolean updated = false;
-        Object oldValue = null;
-        int index = getIndex(key);
+        int index = getBucketIndex(key);
 
-        for (Entry entry : buckets[index]) {
+        ArrayList<Entry> bucket = this.bucket[index];
+        for (Entry entry : bucket) {
             if (key.equals(entry.key)) {
-                oldValue = entry.value;
+                Object oldValue = entry.value;
                 // подменяем значение, если решили внести вместо существующего oldValue
                 entry.value = value;
-                updated = true;
+                return oldValue;
             }
         }
         // если не вносим новое значение, делаем возврат того, что было
-        if (!updated) {
-            buckets[index].add(new Entry(key, value));
-            return null;
-        }
+        bucket.add(new Entry(key, value));
+        size++;
 
-        return oldValue;
+        return null;
     }
 
-    private int getIndex(Object key) {
+    private int getBucketIndex(Object key) {
         // находим, по какому индексу (в каком ведре)  находится элемент
-        return key.hashCode() % buckets.length;
+        return Math.abs(key.hashCode()) % bucket.length;
     }
 
     @Override
     public Object get(Object key) {
-        int index = getIndex(key);
-        for (Entry entry : buckets[index]) {
+        int index = getBucketIndex(key);
+        for (Entry entry : bucket[index]) {
             if (entry.key.equals(key)) {
                 return entry.value;
             }
@@ -60,7 +72,7 @@ public class HashMap implements Map {
 
     @Override
     public boolean containsKey(Object key) {
-        return get(key) != null;
+        return (key==null ? key==null : key.equals(key));
     }
 
     // класс-обертка для хранения ключа и значения
