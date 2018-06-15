36608db6b4eaa970fd04808b8b014a78be938e52
IrinaBabich <36416921+IrinaBabich@users.noreply.github.com>
2018-06-15 23:57:03 +0300

    Update with iterators and generics

--- HashMap.java
+++ HashMap.java
@@ -2,40 +2,40 @@ package com.babich.map;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-public class HashMap implements Map {
+public class HashMap<K, V> implements Map<K, V> {
     private static final int INITIAL_CAPACITY = 5;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private ArrayList<Entry<K, V>>[] buckets;
     private int size;
-    private static ArrayList<Entry>[] bucket;
 
     public HashMap() {
         this(INITIAL_CAPACITY);
     }
 
-    public HashMap(int bucketCount) {
-        if (bucketCount <= 0) {
-            throw new IllegalArgumentException("Bucket size should be > 0 , bucket count =" + bucketCount);
+    public HashMap(int bucketsCount) {
+        if (bucketsCount <= 0) {
+            throw new IllegalArgumentException("Bucket size should be > 0 , bucket count =" + bucketsCount);
         }
-        bucket = new ArrayList[bucketCount];
-        for (int i = 0; i < bucketCount; i++) {
-            bucket[i] = new ArrayList<>();
+        this.buckets = new ArrayList[bucketsCount];
+        for (int i = 0; i < bucketsCount; i++) {
+            buckets[i] = new ArrayList<>();
         }
     }
 
     @Override
-    // если у нас уже был ключ и значение, то когда помещаем новое значение,
-    // происходит апдейт и метод нам возвращает новое значение
-    public Object put(Object key, Object value) {
+// если у нас уже был ключ и значение, то когда помещаем новое значение,
+// происходит апдейт и метод нам возвращает новое значение
+    public V put(K key, V value) {
         boolean updated = false;
-        Object oldValue = null;
+        V oldValue = null;
         int index = getBucketIndex(key);
-        int threshold = (int) (bucket.length * DEFAULT_LOAD_FACTOR);
+
+        int threshold = (int) (buckets.length * DEFAULT_LOAD_FACTOR);
         if (size > threshold) {
             growSize();
         }
-        for (Entry entry : bucket[index]) {
+        for (Entry<K, V> entry : buckets[index]) {
             if (key.equals(entry.key)) {
                 oldValue = entry.value;
                 // подменяем значение, если решили внести вместо существующего oldValue
@@ -43,61 +43,61 @@ public class HashMap implements Map {
                 updated = true;
             }
         }
-
         // если не вносим новое значение, делаем возврат того, что было
         if (!updated) {
-            bucket[index].add(new Entry(key, value));
+            buckets[index].add(new Entry<>(key, value));
             size++;
         }
         return oldValue;
     }
 
-    private void growSize() {
-        int newSize = bucket.length * 2;
-        ArrayList<Entry>[] newBuckets = new ArrayList[newSize];
+    public void growSize() {
+        int newSize = (buckets.length * 2) + 1;
+        ArrayList<Entry<K, V>>[] newBuckets = new ArrayList[newSize];
         for (int i = 0; i < newSize; i++) {
             newBuckets[i] = new ArrayList<>();
         }
     }
 
     // метод копирует одну хеш-мапу в другую
-    public void putAll (HashMap map){
-        Iterator<ArrayList<Entry>> iterator = map.iterator();
+    public void putAll(HashMap<K, V> map) {
+        Iterator<ArrayList<Entry<K, V>>> iterator = map.iterator();
         while (iterator.hasNext()) {
-            ArrayList<Entry> listEntries = iterator.next();
-            for (Entry entry : listEntries) {
+            ArrayList<Entry<K, V>> listEntries = iterator.next();
+            for (Entry<K, V> entry : listEntries) {
                 put(entry.key, entry.value);
             }
         }
     }
 
-    public void putAllIfAbsent(HashMap map) {
-        Iterator<ArrayList<Entry>> iterator = map.iterator();
+    public void putAllIfAbsent(HashMap<K, V> map) {
+        Iterator<ArrayList<Entry<K, V>>> iterator = map.iterator();
         while (iterator.hasNext()) {
-            List<Entry> listEntries = iterator.next();
-            for (Entry entry : listEntries) {
+            ArrayList<Entry<K, V>> listEntries = iterator.next();
+            for (Entry<K, V> entry : listEntries) {
                 putIfAbsent(entry.key, entry.value);
             }
         }
     }
 
     // если ключ не совпадает со значением, возвращает предыдущее значение связанное с ключем
-    public Object putIfAbsent(Object key, Object value) {
-        if (!HashMap.containsKey(key))
-            return HashMap(key, value);
-        else
-            return HashMap.get(key);
+    public V putIfAbsent(K key, V value) {
+        V mapValue = (V) get(key);
+        if (mapValue == null) {
+            mapValue = put(key, mapValue);
+        }
+        return mapValue;
     }
 
     // находим, по какому индексу (в каком ведре)  находится элемент
-    private int getBucketIndex(Object key) {
-        return Math.abs(key.hashCode()) % bucket.length;
+    public int getBucketIndex(K key) {
+        return Math.abs(key.hashCode()) % buckets.length;
     }
 
     @Override
-    public Object get(Object key) {
+    public Object get(K key) {
         int index = getBucketIndex(key);
-        for (Entry entry : bucket[index]) {
+        for (Entry<K, V> entry : buckets[index]) {
             if (entry.key.equals(key)) {
                 return entry.value;
             }
@@ -116,16 +116,16 @@ public class HashMap implements Map {
     }
 
     @Override
-    public boolean containsKey(Object key) {
+    public boolean containsKey(K key) {
         return (key == null ? key == null : key.equals(key));
     }
 
     // класс-обертка для хранения ключа и значения
-    private static class Entry {
-        private Object key;
-        private Object value;
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
 
-        public Entry(Object key, Object value) {
+        public Entry(K key, V value) {
             this.key = key;
             this.value = value;
         }
@@ -136,16 +136,23 @@ public class HashMap implements Map {
     }
 
     private class HashMapIterator implements Iterator {
-        int current;
+        int bucketIndex = 0;
 
+        // возвращает true, если элементы есть
         public boolean hasNext() {
-            return current != bucket.length;
+            if (bucketIndex != buckets.length) {
+                return true;
+            }
+            return false;
         }
 
-        public ArrayList<Entry> next() {
-            int i = current;
-            current++;
-            return bucket[i];
+        // возвращает последующее значение при итерации
+        @Override
+        public Object next() {
+            ArrayList<Entry<K, V>> bucket = buckets[bucketIndex];
+            Object next = bucket.get(bucketIndex);
+            return next;
         }
     }
 }
+
